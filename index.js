d9e0b28f29b60d953e63f00a3cd7a579eb41fd34
ronyn0 <ronyn@librem.one>
2024-07-01 22:35:18 -0400

    added express manually and configured index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+var app = express()
+
+app.get("/", function (request, response) {
+     response.send("Hello World!") 
+})
+app.listen(3000, function () { 
+    console.log("Started application on port %d", 3000) 
+});
